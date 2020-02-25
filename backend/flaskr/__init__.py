92cbe453dcf84e0f4fd6c41cbf66c8fc63294c00
Themba <themba@teamcoda.com>
2020-02-25 01:30:22 +0000

    remove code comments and update documentation

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -78,45 +78,13 @@ def create_app(test_config=None):
                 'questions': current_questions
             })
 
-
-    '''
-    
-    @TODO: 
-    Create an endpoint to handle GET requests for questions, 
-    including pagination (every 10 questions). 
-    This endpoint should return a list of questions, 
-    number of total questions, current category, categories. 
-  
-    TEST: At this point, when you start the application
-    you should see questions and categories generated,
-    ten questions per page and pagination at the bottom of the screen for three pages.
-    Clicking on the page numbers should update the questions. 
-    '''
-
-    '''
-    @TODO: 
-    Create an endpoint to POST a new question, 
-    which will require the question and answer text, 
-    category, and difficulty score.
-
-    TEST: When you submit a question on the "Add" tab, 
-    the form will clear and the question will appear at the end of the last page
-    of the questions list in the "List" tab.  
-    '''
-
-    '''
-   @TODO: 
-   Create a POST endpoint to get questions based on a search term. 
-   It should return any questions for whom the search term 
-   is a substring of the question. 
-
-   TEST: Search by any phrase. The questions list will update to include 
-   only question that include that string within their question. 
-   Try using the word "title" to start. 
-   '''
-
     @app.route('/questions/', methods=['GET', 'POST'])
     def questions():
+        """
+        GET: retrieve all questions
+        POST: search for questions by field question
+        POST: create a new question
+        """
 
         if request.method == 'GET':
             questions = Question.query.all()
@@ -195,7 +163,11 @@ def create_app(test_config=None):
     @app.route('/questions/<int:question_id>/', methods=['GET', 'DELETE'])
     def question(question_id):
 
-        # get requests
+        """
+        GET: retrieve  question by id
+        DELETE: remove question
+        """
+
         if request.method == 'GET':
             question = Question.query.filter(Question.id == question_id).one_or_none()
 
@@ -208,14 +180,6 @@ def create_app(test_config=None):
                     'question': question.format()
                 })
 
-        '''
-         @TODO: 
-         Create an endpoint to DELETE question using a question ID. 
-
-         TEST: When you click the trash icon next to a question, the question will be removed.
-         This removal will persist in the database and when you refresh the page. 
-         '''
-        # delete requests here
         if request.method == 'DELETE':
             try:
                 question = Question.query.filter(Question.id == question_id).one_or_none()
@@ -239,22 +203,9 @@ def create_app(test_config=None):
             except:
                 abort(422)
 
-
-
-    '''
-    @TODO: 
-    Create a POST endpoint to get questions to play the quiz. 
-    This endpoint should take category and previous question parameters 
-    and return a random question within the given category, 
-    if provided, and that is not one of the previous questions. 
-  
-    TEST: In the "Play" tab, after a user selects "All" or a category,
-    one question at a time is displayed, the user is allowed to answer
-    and shown whether they were correct or not. 
-    '''
-
     @app.route('/quizzes/', methods=['POST'])
     def quizzes():
+        """Returns questions given category, excludes questions in previous_questions param"""
 
         body = request.get_json()
         previous_questions = body.get('previous_questions', None)
@@ -280,8 +231,7 @@ def create_app(test_config=None):
                 'question': current_questions[0] if len(current_questions) > 0 else None,
             })
 
-        except Exception as e:
-            print(e)
+        except:
             abort(404)
 
     # error handlers
